774e678c005a7824106dda70b1aacf52a0b3b088
Kermitted <toadbirb@gmail.com>
2022-06-25 19:34:44 -0700

    added red velvet diamond block

--- src/main/java/net/kermitted/tutorialmod/TutorialMod.java
+++ src/main/java/net/kermitted/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.kermitted.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.kermitted.tutorialmod.block.ModBlocks;
 import net.kermitted.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,5 +15,6 @@ public class TutorialMod implements ModInitializer {
 	public void onInitialize() {
 
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
---
+++ src/main/java/net/kermitted/tutorialmod/block/ModBlocks.java
@@ -0,0 +1,32 @@
+package net.kermitted.tutorialmod.block;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.kermitted.tutorialmod.TutorialMod;
+import net.kermitted.tutorialmod.item.ModItemGroup;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModBlocks {
+    public static final Block RED_VELVET_DIAMOND_BLOCK = registerBlock("red_velvet_diamond_block",
+            new Block(FabricBlockSettings.of(Material.AMETHYST).strength(4f).requiresTool()), ModItemGroup.RED_VELVET);
+
+    private static Block registerBlock(String name, Block block, ItemGroup tab) {
+        registerBlockItem(name, block, tab);
+        return Registry.register(Registry.BLOCK, new Identifier(TutorialMod.MOD_ID, name), block);
+    }
+
+    private static Item registerBlockItem(String name, Block block, ItemGroup tab) {
+        return Registry.register(Registry.ITEM, new Identifier(TutorialMod.MOD_ID, name),
+                new BlockItem(block, new FabricItemSettings().group(tab)));
+    }
+
+    public static void registerModBlocks() {
+        TutorialMod.LOGGER.debug("Registering ModBlocks for " + TutorialMod.MOD_ID);
+    }
+}
